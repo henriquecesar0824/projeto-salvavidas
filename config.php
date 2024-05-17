5380dfa4c7269ebaf8b3535b763ebff74ea2dbca
henriquecesar0824 <henrique.fuhr4@gmail.com>
2024-05-17 15:22:16 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,16 @@
-<
+<?php   
+
+    $dbHost = 'localhost';
+    $dbUser = 'root';
+    $dbPass = '';
+    $dbName = 'formulario-henrique';
+
+    $conexao = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+    if ($conexao->connect_error) {
+        die ('Erro'. $conexao->connect_error);
+    }
+    else {
+        die ('Conexao efetuada com sucesso'. $conexao->connect_error);
+    }
+
+?>
